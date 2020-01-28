ef650f9ed2ac8d04c38e92a5c6dc3855787e590c
dimitar.atanasov@scalefocus.com <dimitar.atanasov@scalefocus.com>
2020-01-28 11:06:03 +0200

    setup env

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
-app.get("/", (req, res) => {
-  res.send({ some: "json" });
-});
+const keys = require("./config/keys");
+require("./models/User");
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(err => console.log("Error on start: " + err.stack));
+const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+require("./controlers/authRoutes")(app);
+require("./services/passport");
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
